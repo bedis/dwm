90f92a29f07338e21dbd23efbef6be8b2eff64da
bedis <bedis9@gmail.com>
2012-08-16 07:42:50 +0200

    autostart script module example

---
+++ scripts/autostart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+### Autostart example script
+### could be placed in .config/dwm/ directory
+
+# dropbox
+~/.dropbox-dist/dropboxd &
+
+# network
+wicd-client &
+
+# settings
+xset b off &
+
