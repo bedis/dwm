8a339dfe4a3ce409aa8a32b30bb144c02f946a1c
bedis <bedis9@gmail.com>
2012-10-13 00:50:31 +0200

    dwm_installation_debian script addition

---
+++ scripts/dwm_installation_debian.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Copyright bedis9 <at> gmail <dot> com
+
+cd /usr/src
+rm -rf dwm-6.0
+apt-get source dwm
+
+cd dwm-6.0
+
+wget http://dwm.suckless.org/patches/dwm-6.0-systray.diff
+patch -p1 < dwm-6.0-systray.diff
+
+wget http://dwm.suckless.org/patches/dwm-6.0-pertag.diff
+patch -p1 < dwm-6.0-pertag.diff
+
+wget http://dwm.suckless.org/patches/dwm-azertykey.diff
+patch -p1 < dwm-azertykey.diff
+
+wget http://dwm.suckless.org/patches/dwm-6.0-focusonclick.diff
+patch -p1 < dwm-6.0-focusonclick.diff
+
+mv config.h config.h.orig
+wget https://raw.github.com/bedis/dwm/master/config/config.h
+
+make
+
